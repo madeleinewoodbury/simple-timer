0eff4e303ae11b8e467be3c7f2ef90ec73f63d07
Madeleine Woodbury <randine25@gmail.com>
2019-11-30 17:07:31 -0600

    Add SVG circle for the timer animation

--- index.js
+++ index.js
@@ -2,14 +2,21 @@
 const durInput = document.querySelector('#duration');
 const startBtn = document.querySelector('#start');
 const pauseBtn = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+// Calculate the perimeter of the circle
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
 // Create a new timer
 const timer = new Timer(durInput, startBtn, pauseBtn, {
   onStart() {
     console.log('Timer started');
   },
   onTick() {
-    console.log('Tick');
+    circle.setAttribute('stroke-dashoffset', currentOffset);
+    currentOffset = currentOffset - 50;
   },
   onComplete() {
     console.log('Complete!');
