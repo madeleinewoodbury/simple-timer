97a9586dfc9e8a40c2f3f69dd863dedcabda5112
Madeleine Woodbury <randine25@gmail.com>
2019-11-30 12:20:57 -0600

    Implement the Timer class

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello');
+class Timer {
+  constructor(durInput, startBtn, pauseBtn) {
+    this.durInput = durInput;
+    this.startBtn = startBtn;
+    this.pauseBtn = pauseBtn;
+
+    this.startBtn.addEventListener('click', this.start);
+  }
+
+  start() {
+    console.log('Timer has started');
+  }
+}
+
+// Select DOM elements
+const durInput = document.querySelector('#duration');
+const startBtn = document.querySelector('#start');
+const pauseBtn = document.querySelector('#pause');
+
+// Create a new timer
+const timer = new Timer(durInput, startBtn, pauseBtn);
